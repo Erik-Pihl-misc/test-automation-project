b20c2ebb8eb1a0cd37e4c6cb67d527f629dfc1fe
Erik Pihl <erik.axel.pihl@gmail.com>
2025-12-29 20:44:58 +0100

    Add lin reg test instructions

--- library/test/ml/lin_reg/fixed_test.cpp
+++ library/test/ml/lin_reg/fixed_test.cpp
@@ -1,1 +1,13 @@
 //! @todo Add test cases for the linear regression model here!
+
+//! @note When comparing floating-point values, use EXPECT_NEAR(x, y, <tolerance>) to allow for 
+//! small numerical differences. 
+//! For example, to check that x is within ±1e-6 of y, write EXPECT_NEAR(x, y, 1e-6).
+
+//! @todo Add a "happy path" test that verifies the model behaves correctly when given ideal input data.
+//! This test should confirm that training and prediction work as expected when all arguments are 
+//! valid and noise-free.
+
+//! @todo Add tests to verify that the model does not get trained if any input argument is invalid, 
+//! e.g., if the number of complete training sets is 0, the epoch count is 0, or the learning rate 
+//! is less than or equal to 0. Also verify that the model always predicts 0 when untrained.
